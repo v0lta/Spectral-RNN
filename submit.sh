6629daa4ced624d9ce3808e698293ee5029fa727
moritz <wolter@cs.uni-bonn.de>
2019-06-17 16:09:34 +0200

    cleanup.

--- submit.sh
+++
@@ -1,4 +0,0 @@
-#block(name=CVPR_fft_pred, threads=2, memory=7500, subtasks=1, hours=96, gpus=1)
-    echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    ipython synthetic_signal_test.py
-   
--- tmux.sh
+++
@@ -1,5 +0,0 @@
-#block(name=tmux-interactive, threads=2, memory=7500, subtasks=1, hours=48, gpus=1)
-    echo "process subtask $SUBTASK_ID of $N_SUBTASKS"
-    tmux new -s terminal -d
-    sleep 24h
-   
