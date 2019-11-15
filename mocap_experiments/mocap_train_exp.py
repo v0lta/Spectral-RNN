26e1adeae1f596d732330d00b80ff459cc4c4542
moritz <wolter@cs.uni-bonn.de>
2019-11-15 14:16:28 +0100

    re-re-run.

--- mocap_experiments/mocap_train_exp.py
+++ mocap_experiments/mocap_train_exp.py
@@ -98,7 +98,7 @@ else:
 
 lpd_lst = []
 # define a list of experiments.
-for consistency_loss_weight in [0.001, 0.0]:
+for consistency_loss_weight in [0.005, 0.001, 0.0]:
     for fft in [True]:
         cpd = pd.copy()
         cpd['consistency_loss_weight'] = consistency_loss_weight
